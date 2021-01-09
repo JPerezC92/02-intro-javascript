94df05b9f4e78b4c968bc0eb6abbe995e649f5b6
PEREZ CASTRO PHILIP JUNIOR <jperez.c92@gmail.com>
2021-01-09 01:26:07 -0500

    const-let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+// Variables y constantes
+const nombre = "Philip";
+const apellido = "Pérez";
+
+let valorDado = 4;
+valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+// var No se debe usar
+if (true) {
+  const nombre = "Jose";
+  console.log(nombre);
+}
+
+console.log(valorDado);
