e269299a320040c0418778c07ba6d6c7e2153761
PEREZ CASTRO PHILIP JUNIOR <jperez.c92@gmail.com>
2021-01-10 15:49:12 -0500

    promesas

--- src/bases/08-import-export.js
+++ src/bases/08-import-export.js
@@ -1,15 +1,17 @@
-import heroes, { owners } from "./data/heroes";
+// import heroes, { owners } from "./data/heroes";
+// import heroes, { owners } from "../data/heroes";
+import heroes from "../data/heroes";
 
 // console.log(heroes);
 
-const getHeroeById = (id) => {
+export const getHeroeById = (id) => {
   return heroes.find((hero) => hero.id === id);
 };
-console.log(getHeroeById(2));
+// console.log(getHeroeById(2));
 
-const getHeroeByOwner = (owner) => {
+export const getHeroeByOwner = (owner) => {
   return heroes.filter((hero) => hero.owner === owner);
 };
 
-console.log(getHeroeByOwner("DC"));
-console.log(owners);
+// console.log(getHeroeByOwner("DC"));
+// console.log(owners);
